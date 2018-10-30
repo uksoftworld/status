5795734198009e4fc0d102255ff384b0c36eff27
BestCoderz <utk.bkkg@gmail.com>
2018-10-30 11:05:16 +0530

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -12,14 +12,13 @@ from discord.voice_client import VoiceClient
 from discord import Game, Embed, Color, Status, ChannelType
 
 
-Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
-client = Bot(description="DarkBot Bot is best", command_prefix="d!", pm_help = True)
+client = Bot(description="Our Bot is best", command_prefix="d!", pm_help = True)
 client.remove_command('help')
 
 
 async def status_task():
     while True:
-        await client.change_presence(game=discord.Game(name='for d!help'))
+        await client.change_presence(game=discord.Game(name='for $help'))
         await asyncio.sleep(5)
         await client.change_presence(game=discord.Game(name='with '+str(len(set(client.get_all_members())))+' users'))
         await asyncio.sleep(5)
